61dae69054fac6c9ceb6877e0b3838910e19ab21
Mansoor <mansoor.mujawar@gmail.com>
2019-02-13 12:59:37 -0600

    daily coding 2nd problem in progress

--- dailycodingproblem/list_excercises.py
+++ dailycodingproblem/list_excercises.py
@@ -12,7 +12,7 @@ class DailyCoding:
     @staticmethod
     def product_of_array_except_current_index(array):
         """
-        ## Question - This problem was asked by Uber.
+        ## Problem #2 - This problem was asked by Uber.
 
                 Given an array of integers, return a new array such that
                 each element at index i of the new array is the product of all the numbers
@@ -21,7 +21,7 @@ class DailyCoding:
                 For example:
                 If our input is [3, 2, 1], the expected output would be [2, 3, 6].
 
-        ## Solution Analysis about problem statement.
+        ## Problem Analysis -
                 # Given  -> Integer array [3, 2, 1]
                 # When   -> multiply each element of array except current index
                 # Then   ->  [2, 3, 6]
@@ -50,4 +50,46 @@ class DailyCoding:
         print("Product array is: -> ", end='')
         print(product_array)
         return product_array
+
+    @staticmethod
+    def is_any_two_numbers_add_up_to_k(lst, k):
+        """
+        ## Problem #1 - This problem was asked by Google.
+
+            Given a list of numbers and a number k,
+            return whether any two numbers from the list add up to k.
+            For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17
+
+        ## Problem Analysis -
+           # Given -> number list
+           # And   -> number k
+           # When  -> any two numbers add up to k
+           # Then  -> return True
+           # Else  -> return False
+           # Task  ->
+                1. needs to loop through numbers to get index and numbers  solution -> for loop with enumerate function
+                2. at current index get the list of rest numbers           solution -> get nested for loop
+                3. rest number list should have (start, end)          solution -> range(current_index + 1, len(lst) + 1)
+                4. need to avoid list outofbound error                Solution -> 2nd last ele don't enter into 2nd loop
+                5. check two numbers add up to k                      solution -> current index ele + next index ele
+                6. return True if adds up                             solution -> return True
+                7. else return False                                  solution -> return False
+
+        :return: boolean True or False
+        """
+        for index, first_element in enumerate(lst):
+            if index != (len(lst) - 1):
+                for i, second_element in enumerate(lst[index+1:]):
+                    if first_element + second_element == k:
+                        return True
+                        break
+
+            elif first_element + lst[-1] == k:
+                return True
+                break
+
+            else:
+                return False
+
+
 
--- test/daily_coding_test.py
+++ test/daily_coding_test.py
@@ -15,14 +15,21 @@ from proboscis import TestProgram
 from dailycodingproblem.list_excercises import DailyCoding
 
 
-@test
+#@test
 def test_product_of_array_except_current_index():
-    input_array = [[3, 2, 1], [1, 1, 1]]
+    input_ = [[3, 2, 1], [1, 1, 1]]
     expected = [[2, 3, 6], [1, 1, 1]]
 
-    for actual_array, expected_array in zip(input_array, expected):
+    for actual_array, expected_array in zip(input_, expected):
         product_array = DailyCoding.product_of_array_except_current_index(actual_array)
         assert_equal(expected_array, product_array)
+
+@test
+def test_is_any_two_numbers_add_up_to_k():
+    lst = [10, 15, 3, 5]
+    k = 17
+    is_add_up = DailyCoding.is_any_two_numbers_add_up_to_k(lst, k)
+    assert_equal(is_add_up, True)
 
 
 TestProgram().run_and_exit()
